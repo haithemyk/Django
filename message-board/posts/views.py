3072d1147696543743869ee4bcf9e3bae5ae16d0
haithemyk <haithemyk@outlook.com>
2023-06-15 21:13:55 +0100

    init Message board app

---
+++ message-board/posts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# Create your views here.
+from posts.models import Post
+from django.views.generic import ListView
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
